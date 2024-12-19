bd4ce9b40ff48ecffdcf77dea938fe1fbaf43fae
Marin Dedvukaj <marin.dedvukaj2005@gmail.com>
2024-12-19 11:12:54 +0100

    added comments

--- DataCollect.py
+++ DataCollect.py
@@ -5,7 +5,8 @@ import time
 class Reciver():
 
     #connect to arduino
-    def portScanner(self):
+    def arduinoConnect(self):
+        baudRate = 9600
         #set a variable that contains port where arduino is tored to None 
         self.arduinoPort = None
         #collect all ports scanned to a variable 
@@ -15,40 +16,48 @@ class Reciver():
             if "Arduino" in port.description or "Arduino" in port.manufacturer:
                 #add arduino port to the variable and then 
                 self.arduino_port = port.device
-                pass
+                break
+        #check if arduino was found or not 
         if not self.arduinoPort:
             print("No Arduino found.")
+        #inicialize connection with arduino and wait it to be properly established before returning
         self.ser = serial.Serial(self.arduino_port,self.baudRate,timeout=10)
         time.sleep(2)
         return None
     
-
+    #connect to the arduino on cleass init 
     def __init__(self):
-        self.portScanner()
-        self.baudRate = 9600
+        self.arduinoConnect()
+
         pass
 
-
+    #recive data from arduino connection
     def receive(self):
         try:
+            #recive data, decode for special characters, removewhitespace, split at the / to obtain a list of the diferent data
             data = self.ser.readline().decode("utf-8").strip().split("/")
             pass
         except Exception as exc:
+            #if we encounter error dael with it in case of arduino not connected reconnect
             print(exc)
-            self.portScanner()
+            self.arduinoConnect()
 
         return data
 
+
+#run func if file is excecuted directly 
 def main():
+    #inicialize class
     rec = Reciver()
     print("starting reading")
+    #loop to recive data and print untill ctr + c is pressed 
     while True:
         try:
             print(rec.receive())
         except KeyboardInterrupt:
             print("Ending reading....")
-    
-    return
+
+    return None 
 
 if __name__ == "__main__":
     main()
