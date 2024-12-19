ee333519afc27c0f78d1ccfb5486248ee78443d7
Marin Dedvukaj <marin.dedvukaj2005@gmail.com>
2024-12-19 10:47:51 +0100

    random chenges

--- DataCollect.py
+++ DataCollect.py
@@ -3,25 +3,31 @@ import serial
 import time
 
 class Reciver():
-    
-    def __init__(self):
-        self.portScanner()
-        self.baudRate = 9600
-        pass
 
-    #scann the ports on the computer and 
+    #connect to arduino
     def portScanner(self):
+        #set a variable that contains port where arduino is tored to None 
         self.arduinoPort = None
+        #collect all ports scanned to a variable 
         ports = serial.tools.list_ports.comports()
+        #iterate through ports to find the one connected to arduino
         for port in ports:
             if "Arduino" in port.description or "Arduino" in port.manufacturer:
+                #add arduino port to the variable and then 
                 self.arduino_port = port.device
-                return None
+                pass
         if not self.arduinoPort:
             print("No Arduino found.")
         self.ser = serial.Serial(self.arduino_port,self.baudRate,timeout=10)
         time.sleep(2)
         return None
+    
+
+    def __init__(self):
+        self.portScanner()
+        self.baudRate = 9600
+        pass
+
 
     def receive(self):
         try:
